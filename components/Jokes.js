a2d691cb1fbf7e9c3b48ffde80d2d2e89009c184
Emily Stroud <emilystroud93@gmail.com>
2022-07-12 19:38:58 -0500

    trying to change button text

--- components/Jokes.js
+++ components/Jokes.js
@@ -2,17 +2,21 @@ import { useState } from 'react';
 import { PropTypes } from 'prop-types';
 
 function Jokes({ title }) {
-  const [joke, setJoke] = useState(0);
+  const [joke, setJoke] = useState();
 
   const handleClick = () => {
     setJoke((prevState) => prevState);
   };
+
+  // const updateJoke = (newJoke) => setJoke(newJoke);
+  // updateJoke();
 
   return (
     <>
       <h1>{title}</h1>
       <h2>{joke}</h2>
       <button type="button" onClick={handleClick}>Get a Punchline</button>
+      { joke ? <button type="button" onClick={() => setJoke((prevState) => prevState)}>Get A Punchline</button> : '' }
     </>
   );
 }
